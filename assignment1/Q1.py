916d8d05254f7cdc7a4d548d51d6a9510a91a13e
ksabhiram16 <kssabhiram23@gmail.com>
2026-01-15 16:28:03 +0530

    Create Q1.py

---
+++ assignment1/Q1.py
@@ -0,0 +1,14 @@
+def count_message(msg, count=0):
+    """
+    This function accepts a message and a default count.
+    Since integers are immutable, the count resets on every function call.
+    """
+    count += 1
+    print(f"Message: {msg}, Count: {count}")
+    return count
+
+
+# Sample calls
+count_message("heya")
+count_message("hello again")
+count_message("third call")
