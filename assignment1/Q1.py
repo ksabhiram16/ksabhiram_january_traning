3e36a94cbe31568ab30217f6188dcd9d9f6eff42
KS Abhiram <kssabhiram23@gmail.com>
2026-01-26 22:05:31 +0530

    Q1.py adding

--- assignment1/Q1.py
+++ assignment1/Q1.py
@@ -6,9 +6,6 @@ def count_message(msg, count=0):
     count += 1
     print(f"Message: {msg}, Count: {count}")
     return count
-
-
-
 count_message("heya")
 count_message("hello again")
 count_message("third call")
--- assignment1/Q3.py
+++ assignment1/Q3.py
@@ -3,9 +3,6 @@ def save_error(error, errors=None):
         errors = []
     errors.append(error)
     return errors
-
-
-
 print(save_error("E1"))
 print(save_error("E2"))
 print(save_error("E3"))
